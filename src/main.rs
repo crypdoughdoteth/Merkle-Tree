da180c7722e16752d608ec29103653d72e8f7a88
crypdough.eth <106392730+crypdoughdoteth@users.noreply.github.com>
2023-01-21 22:21:42 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -24,15 +24,16 @@ struct Tree{
         let mut parent_nodes: Vec<Node> = Vec::new();
         let mut children_nodes: Vec<Node> = leafs.iter().map(|x|{
            let x_hashed = Leaf::hashLeaf(x.data.as_bytes());
-            Node::new(Box::new(None), Box::new(None), x_hashed)
+            Node::new(Box::new(None), Box::new(None),Box::new(None), x_hashed)
         }).collect();
+
         //check length of leaf nodes, if there is an odd number of leaf nodes, copy the odd element and push it to the array
         let len = &children_nodes.len();
         if len % 2 == 1 { 
             children_nodes.push(children_nodes[len -1].clone()); 
             children_nodes[len-1].copied = true;
         }
-         
+
         let mut n = children_nodes.len();
         self.depth = 1; 
         //main loop, make sure there is at least two elemnts left, otherwise we've hit the root
@@ -45,23 +46,35 @@ struct Tree{
                 let array = [&children_nodes[i].hash, &children_nodes[i + 1].hash];
                 //hash nodes together
                 let new_node_hash = Node::hashNodes(array);
-                parent_nodes.push(
+
+                let new_node =                    
                     Node{ 
                         left_child: Box::new(Some(children_nodes[i].clone())),
                         right_child: Box::new(Some(children_nodes[i + 1].clone())),
+                        parent: Box::new(None),
                         hash: new_node_hash,
                         copied: false, 
                         index: o,
-                    });
-                self.nodes.push(
-                    Node{ 
-                        left_child: Box::new(Some(children_nodes[i].clone())),
-                        right_child: Box::new(Some(children_nodes[i + 1].clone())),
-                        hash: new_node_hash,
-                        copied: false, 
-                        index: o,
-                    }
-                );
+                    };
+                parent_nodes.push(new_node.clone());
+                self.nodes.push(new_node.clone());
+
+                children_nodes[i] = Node{
+                    left_child: children_nodes[i].left_child.clone(),
+                    right_child: children_nodes[i].right_child.clone(),
+                    parent: Box::new(Some(new_node.clone())),
+                    hash: children_nodes[i].hash,
+                    copied: children_nodes[i].copied, 
+                    index: children_nodes[i].index,
+                };
+                children_nodes[i + 1] = Node{
+                    left_child: children_nodes[i+1].left_child.clone(),
+                    right_child: children_nodes[i+1].right_child.clone(),
+                    parent: Box::new(Some(new_node.clone())),
+                    hash: children_nodes[i+1].hash,
+                    copied: children_nodes[i+1].copied, 
+                    index: children_nodes[i+1].index,
+                };
                 o += 1; 
                 //increment loop by 2
                 i += 2;
@@ -83,6 +96,7 @@ struct Tree{
                 self.root = Node{                        
                     left_child: Box::new(Some(self.nodes[len - 2].clone())),
                     right_child: Box::new(Some(self.nodes[len - 1].clone())),
+                    parent: Box::new(None),
                     hash: Node::hashNodes([&self.nodes[len - 2].hash.clone(), &self.nodes[len - 1].hash.clone()]),
                     copied: false,
                     index: self.nodes.len() + 1,
@@ -104,6 +118,87 @@ struct Tree{
     fn get_root(self) -> [u8;32]{
         self.root.hash
     }
+
+    fn generate_proof(self, leaf: Node) -> Vec<[u8;32]> {
+        // start with leaf node => use pointer to parent node. Parent node has two children, one of which is the other hash that made it. Push hash to proof vec =>
+        // Once the first hash is located, we then may query the parent of the first non-leaf node => search children for matching hash =>  push has to proof vec => repeat until root
+        
+        let mut parent = leaf.parent;
+        let mut proof_hashes:  Vec<[u8;32]> = vec![];
+        
+
+        // start with the leaf
+        let leaf_hash = leaf.hash;
+        
+        
+        let mut current_parent_node = parent.clone(); 
+        let mut current_child_node:Node;
+        let mut current_hash: [u8;32] = leaf.hash.clone();
+        
+        loop{
+            //break condition, found root (parent for current node returns as None)
+            match *current_parent_node.clone(){
+                Some(x) => {
+                    match *x.parent{
+                        None => return proof_hashes,
+                        _ => (),
+                    }
+                },
+                None => (),
+            }
+            
+            match *current_parent_node.clone(){
+                Some(parent_node) => {
+
+                    //match left and right child hashes to see if it matches current hash (hash of child we are using to reconstruct the node with)
+                    //look for the hash that doesn't match itself 
+                    let left = parent_node.left_child.clone();
+                    let right = parent_node.right_child.clone();
+
+                    match *left{
+                        Some(the_left_child) => {
+                            if &the_left_child.hash != &current_hash.clone(){
+                                proof_hashes.push(the_left_child.hash);
+                                //set new parent node -- the parent of the current parent, set current parent's hash to current hash
+                                current_parent_node = parent_node.parent.clone();
+                                match *current_parent_node.clone(){
+                                    Some(x) => {
+                                        current_hash = x.hash;
+                                    },
+                                    None => (),
+                                }
+                            }
+                        },
+                        None => (),
+                    }
+                    
+                    match *right{
+                        Some(the_right_child) => {
+                            if &the_right_child.hash != &current_hash.clone(){
+                                proof_hashes.push(the_right_child.hash);
+                                current_parent_node = parent_node.parent;
+
+                                match *current_parent_node.clone(){
+                                    Some(x) => {
+                                        current_hash = x.hash;
+                                    },
+                                    None => (),
+                                }
+                            }
+                        },
+                        None => (),
+                    }
+
+                },
+
+                None => (),
+            }    
+        
+        }  
+    }
+    
+    
+    //okay, I gotta refactor this to be cleaner. I don't need this extra loop sequence
     fn search_tree(self, intermediate_hashes: Vec<[u8;32]>, leaf: [u8;32]) -> bool{
         //stategy: root => match ((left child || right child), intermediate_hash[i])  => node @ matched index ... => ... =>    
         
@@ -157,7 +252,6 @@ struct Tree{
                 },
                 None => ()
              };
-            if found == true { return found; }
              match &*node.right_child {
                 Some(x) => {
                     if x.hash == leaf{
@@ -182,14 +276,15 @@ struct Tree{
 struct Node{
     left_child: Box<Option<Node>>,
     right_child: Box<Option<Node>>,
+    parent: Box<Option<Node>>,
     hash: [u8;32],
     copied: bool,
     index: usize,
 }
 
 impl Node{
-    fn new(lc: Box<Option<Node>>, rc: Box<Option<Node>>, h: [u8;32]) -> Node{
-        Self{left_child: lc, right_child: rc, hash: h, copied: false, index : 0}
+    fn new(lc: Box<Option<Node>>, rc: Box<Option<Node>>, p:Box<Option<Node>>, h: [u8;32]) -> Node{
+        Self{left_child: lc, right_child: rc, parent: p, hash: h, copied: false, index : 0}
     }
     fn get_left(self) -> Box<Option<Node>>{
         self.left_child
