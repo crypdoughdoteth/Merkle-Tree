ee88a883a0c6fbc99ff4da2df683a3b5923d3091
crypdoughdoteth <zkorn1@icloud.com>
2024-06-16 21:31:05 -0400

    "Refactor proof generation"

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,11 @@ impl MerkleTree {
         Self { elements }
     }
 
+    pub fn new_from_hashes(mut elements: Vec<HashValue>) -> MerkleTree {
+        MerkleTree::pad_elements(&mut elements);
+        Self { elements }
+    }
+
     fn pad_elements<T: Default + Clone>(elements: &mut Vec<T>) {
         let len = elements.len();
         if len.is_power_of_two() {
@@ -68,48 +73,49 @@ impl MerkleTree {
     }
 
     pub fn generate_proof(&self, index: usize) -> (MerkleRoot, MerkleProof) {
-        let (root, proof, _) = (0..(self.elements.len() / 2) - 1).into_iter().fold(
-            (self.elements.clone(), MerkleProof::new(), index),
-            |(mut hashes, mut proof, mut idx), _| {
-                let proof_element = hashes[idx];
-                let new_set = hashes
-                    .into_par_iter()
-                    .chunks(2)
-                    .enumerate()
-                    .fold(
-                        || (Vec::new(), Vec::new(), 0),
-                        |mut acc, (i, e)| {
-                            if e.contains(&proof_element) && e.len() == 2 {
-                                if e[0] == proof_element {
-                                    acc.0.push(NodeLocation::Right(e[1]));
-                                } else {
-                                    acc.0.push(NodeLocation::Left(e[0]));
-                                }
-                                acc.2 = i;
-                            }
-                            if e.len() == 2 {
-                                acc.1.push(Self::concat_hashes(&e[0], &e[1]))
-                            }
-                            acc
-                        },
-                    )
-                    .reduce(
-                        || (Vec::new(), Vec::new(), 0),
-                        |mut acc, x| {
-                            acc.0.extend_from_slice(&x.0);
-                            acc.1.extend_from_slice(&x.1);
-                            acc.2 += x.2;
-                            acc
-                        },
-                    );
-                hashes = new_set.1;
-                proof.0.extend_from_slice(&new_set.0);
-                idx = new_set.2;
-                (hashes, proof, idx)
-            },
-        );
+        let mut elements = self.elements.clone();
+        let mut proof = MerkleProof::new();
+        let mut idx: usize = index;
+        loop {
+            if elements.len() == 1 {
+                break;
+            }
+            let proof_element = elements[idx];
 
-        (MerkleRoot(root[0]), proof)
+            let (proof_elements, new_set, index) = elements
+                .into_par_iter()
+                .chunks(2)
+                .enumerate()
+                .fold(
+                    || (Vec::new(), Vec::new(), 0usize),
+                    |mut acc, (i, e)| {
+                        if e.contains(&proof_element) {
+                            if e[0] == proof_element {
+                                acc.0.push(NodeLocation::Right(e[1]));
+                            } else {
+                                acc.0.push(NodeLocation::Left(e[0]));
+                            }
+                            acc.2 = i;
+                        }
+                        acc.1.push(Self::concat_hashes(&e[0], &e[1]));
+                        acc
+                    },
+                )
+                .reduce(
+                    || (Vec::new(), Vec::new(), 0),
+                    |mut acc, x| {
+                        acc.0.extend_from_slice(&x.0);
+                        acc.1.extend_from_slice(&x.1);
+                        acc.2 += x.2;
+                        acc
+                    },
+                );
+
+            elements = new_set;
+            idx = index;
+            proof.extend_from_slice(&proof_elements);
+        }
+        (MerkleRoot(elements[0]), proof)
     }
 
     pub fn validate_proof<T: AsRef<[u8]> + ?Sized>(
@@ -147,6 +153,10 @@ impl MerkleProof {
 
     pub fn push(&mut self, value: NodeLocation) {
         self.0.push(value)
+    }
+
+    pub fn extend_from_slice(&mut self, proof_elements: &[NodeLocation]) {
+        self.0.extend_from_slice(&proof_elements)
     }
 }
 
@@ -196,6 +206,32 @@ mod test {
         assert_eq!(MerkleRoot(root), tree.merkle_root());
         let (root, proof) = tree.generate_proof(3);
         assert!(tree.validate_proof(&root, "d", &proof));
+    }
 
+    #[test]
+    fn sixteen() {
+        let values = vec!["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+
+        let tree = MerkleTree::new(values);
+        let first = MerkleTree::concat_hashes(&tree.elements[0], &tree.elements[1]);
+        let second = MerkleTree::concat_hashes(&tree.elements[2], &tree.elements[3]);
+        let third = MerkleTree::concat_hashes(&tree.elements[4], &tree.elements[5]);
+        let fourth = MerkleTree::concat_hashes(&tree.elements[6], &tree.elements[7]);
+        let fifth = MerkleTree::concat_hashes(&tree.elements[8], &tree.elements[9]);
+        let sixth = MerkleTree::concat_hashes(&tree.elements[10], &tree.elements[11]);
+        let seventh = MerkleTree::concat_hashes(&tree.elements[12], &tree.elements[13]);
+        let eighth = MerkleTree::concat_hashes(&tree.elements[14], &tree.elements[15]);
+        // second row
+        let nineth = MerkleTree::concat_hashes(&first, &second);
+        let tenth = MerkleTree::concat_hashes(&third, &fourth);
+        let eleventh = MerkleTree::concat_hashes(&fifth, &sixth);
+        let twelfth = MerkleTree::concat_hashes(&seventh, &eighth);
+        // third
+        let thirteenth = MerkleTree::concat_hashes(&nineth, &tenth);
+        let fourteenth = MerkleTree::concat_hashes(&eleventh, &twelfth);
+        let root = MerkleTree::concat_hashes(&thirteenth, &fourteenth);
+        assert_eq!(MerkleRoot(root), tree.merkle_root());
+        let (root, proof) = tree.generate_proof(6);
+        assert!(tree.validate_proof(&root, "g", &proof));
     }
 }
